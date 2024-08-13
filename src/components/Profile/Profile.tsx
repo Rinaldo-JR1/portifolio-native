6d5a9d8a4f7b6160fab9c662d68edf23259429ad
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-08-12 22:27:46 -0300

    My profile

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import rinaldo from "../../../assets/profile/rinaldo.jpeg";
+
+import { styles } from './Styles';
+
+export function Profile() {
+    return (
+        <View style={styles.container}>
+            <Image source={rinaldo} style={{ width: 200, height: 200, borderRadius: 100 }} />
+            <Text style={styles.name}>Rinaldo Uchoa</Text>
+            <Text style={styles.description}>
+                Desenvolvedor Junior Full stack
+            </Text>
+        </View>
+    );
+}
---
+++ src/components/Profile/Styles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    name: {
+        padding: 20,
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    description: {
+        padding: 20,
+        fontSize: 15,
+    }
+});
